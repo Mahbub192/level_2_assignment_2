a21d644c432b687a0e22b55403eae875abed2bfe
Mahbub Ali <mahbubaligub192@gmail.com>
2023-11-24 08:22:49 +0600

    connect to the mongodb and run sussfully

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
 
-//parser
-app.use(express.json());
-app.use(cors());
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+const app: Application = express()
 
-export default app;
+//parsers
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10
+  res.send(a)
+})
+
+export default app
